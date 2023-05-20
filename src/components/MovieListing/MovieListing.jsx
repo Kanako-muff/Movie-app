4d9697646c01b7ad7d2f72ce57d08e08960db779
Kanako-muff <kanako.design00@gmail.com>
2023-05-20 12:21:12 -0700

    fixed err

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { getAllMovies, getAllShows } from '../../features/movies/movieSlice';
-import MovieCard from "../MovieCard/MovieCard"
-import "./MovieListing.scss"
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieListing.scss";
 import Slider from "react-slick";
-import { Settings } from '../../common/settings';
+import { Settings } from "../../common/settings";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
-  let renderMovies, renderShows = "";
+  let renderMovies,
+    renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
@@ -17,23 +18,21 @@ const MovieListing = () => {
         <MovieCard key={index} data={movie} />
       ))
     ) : (
-      <div className = "movies-error">
+      <div className="movies-error">
         <h3>{movies.Error}</h3>
       </div>
     );
   renderShows =
     shows.Response === "True" ? (
-      shows.Search.map((movie, index) => (
-        <MovieCard key={index} data={movie} />
-      ))
+      shows.Search.map((movie, index) => <MovieCard key={index} data={movie} />)
     ) : (
-      <div className = "shows-error">
+      <div className="shows-error">
         <h3>{shows.Error}</h3>
       </div>
     );
 
   return (
-    <div className="movie-werapper">
+    <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">
@@ -47,7 +46,7 @@ const MovieListing = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
-export default MovieListing
+export default MovieListing;
